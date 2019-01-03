bd0455b3c37293326a1c930bc166463352c85e52
wangtianrun <m17888819538@163.com>
2019-01-03 11:43:58 +0800

    layout design

---
+++ app/src/main/java/cn/edu/pku/wangtianrun/classroomreservation/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.edu.pku.wangtianrun.classroomreservation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_info);
+    }
+}
